a402f4cf8d172d02c603edaae9bf25a421d25b03
Flavio Lucio <flaviolucio10@gmail.com>
2021-09-12 21:44:07 -0300

    Finalizada a alteração dos cards

--- src/App.vue
+++ src/App.vue
@@ -68,7 +68,7 @@
 
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-app-bar-title>Empreendimentos</v-app-bar-title>
+      <v-app-bar-title>Empreendimentos API</v-app-bar-title>
 
       <v-spacer></v-spacer>
 
--- src/components/Users/User.vue
+++ src/components/Users/User.vue
@@ -25,56 +25,29 @@
         align="center"
         class="mx-0"
       >
-        <v-rating
-          :value="4.5"
-          color="amber"
-          dense
-          half-increments
-          readonly
-          size="14"
-        ></v-rating>
-
-        <div class="grey--text ms-4">
-          4.5 (413)
+ 
+        <div class="grey--text">
+            <v-row align="center" class="mx-0"> 
+                <b>Id:</b> {{user.id}}   <div class="ms-5"><b>Created At:</b> {{user.createdAt}} </div> 
+        
+            </v-row>
         </div>
       </v-row>
 
-      <div class="my-4 text-subtitle-1">
-        $ • Italian, Cafe
+      <br>
+      <div class="my-3 text-subtitle-1">
+        <b>Phone:</b> {{user.phone}}
       </div>
 
-      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+
+        <v-divider class="mx-4"></v-divider>
+        <br>
+
+        <div><b>Address:</b> {{user.address}} | <b>Number:</b> {{user.number}} </div>
+        <div><b>Zip Code:</b> {{user["zip-code"]}} </div>
+        <div><b>Lat:</b> {{user.lat}} | <b>Long:</b> {{user.long}}</div>
     </v-card-text>
-
-    <v-divider class="mx-4"></v-divider>
-
-    <v-card-title>Tonight's availability</v-card-title>
-
-    <v-card-text>
-      <v-chip-group
-        v-model="selection"
-        active-class="deep-purple accent-4 white--text"
-        column
-      >
-        <v-chip>5:30PM</v-chip>
-
-        <v-chip>7:30PM</v-chip>
-
-        <v-chip>8:00PM</v-chip>
-
-        <v-chip>9:00PM</v-chip>
-      </v-chip-group>
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn
-        color="deep-purple lighten-2"
-        text
-        @click="reserve"
-      >
-        Reserve
-      </v-btn>
-    </v-card-actions>
+    
   </v-card>
   
   </div>
@@ -83,7 +56,6 @@
 <script>
 export default {
     props:['user']
-
 }
 </script>
 
